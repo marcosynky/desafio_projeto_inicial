150efff644b66edfa12930e33d737cf99738256e
M-Alpha <165909014+marcosynky@users.noreply.github.com>
2025-02-19 15:52:30 -0300

    commit

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-alert('Boas vindas ao jogo do número secreto');//mostrar uma caixa de alerta com a mensagem de boas vindas//
-let chute = prompt('Escolha um número entre 1 e 10'); //pedir para o usuário escolher um número entre 1 e 10//
-let numeroSecreto = 2;//definir um número secreto//
-if (chute == numeroSecreto) { //se o número escolhido for igual ao número secreto//
-    alert(`Acertou você descobriu o número secreto ${numeroSecreto}` );//mostrar uma caixa de alerta com a mensagem de acertou//
-} else { //se o número escolhido for diferente do número secreto//
-    alert('Errou!');//mostrar uma caixa de alerta com a mensagem de errou//
-}
+alert('Boas vindas ao nosso site!');
 
-let mensagemDeFinalizacao = 'Obrigado por jogar';//definir uma mensagem de finalização//
-alert(mensagemDeFinalizacao);//mostrar uma caixa de alerta com a mensagem de finalização//
+var nome = 'Lua';
+var idade = 25;
+var numeroDeVendas = 50;
+var saldoDisponivel = 1000;
 
-alert('Bem-vindo ao jogo de Perguntas e Respostas!')
-alert('Responta a pergunta!');
-let perg = prompt('Quantas Horas tem em um dia?');
-let rescorrta = 24;
-if (perg == rescorrta) {
-    alert('Você acertou a pergunta,Parabéns!');
-} else {
-    alert(`Você errou a pergunta, a resposta era ${rescorrta}`);
-}
-
-let idade = prompt("Digite sua idade:");
+alert('Error!Preencha todos os campos');
+var mensagemDeErro = 'Error!Preencha todos os campos';
+alert(mensagemDeErro);
+var nome = prompt('Qual seu nome?');
+var idade = prompt('Qual sua idade?');
 
 if (idade >= 18) {
-  console.log("Você é maior de idade.");
+  alert('Pode tirar CNH');
 } else {
-  console.log("Você é menor de idade.");
+  alert('Não pode tirar CNH');
 }
-
-
 
 
 
