f14fdd70dacda28d17de472c4cad952463b9c2fb
M-Alpha <165909014+marcosynky@users.noreply.github.com>
2025-02-19 15:59:44 -0300

    commit

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor iniciado!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
 alert('Boas vindas ao nosso site!');
 
 var nome = 'Lua';
